866aa58e5854a99b8caddcbdaef881f2d1d15c65
stev0531 <stev0531@morris.umn.edu>
2018-09-05 12:45:01 -0500

    Add to `wrap_contents.sh` to wrap given files
    It now takes the file given and wraps it in the header
    and footer of the supplied name, and then stores it in
    the last file given, either overwriting what's already
    there or creating a new file with the name given.

--- wrap_contents.sh
+++ wrap_contents.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/bash
+#Wraps given text in the header and footer provided,
+#overwriting the given file, if it exists.
 
+#Elizabeth Stevens
+#University of Minnesota Morris
+#September 2018
+
+
+middle=$1
+head_foot_name=$2
+new_file=$3
+
+cat $head_foot_name"_header.html" > $new_file
+cat $middle >> $new_file
+cat $head_foot_name"_footer.html" >> $new_file
 
